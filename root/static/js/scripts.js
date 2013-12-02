3ef521618ebb1b121d473e20daeea39798660cec
Cristiano Faustino <cristiadu@gmail.com>
2013-12-02 05:11:15 -0200

    Alter the product list page

--- root/static/js/scripts.js
+++ root/static/js/scripts.js
@@ -2,3 +2,9 @@
 $('#orderby').change(function () {
     location.href = location.href.replace(location.search, '') + '?sort=' + $('#orderby').val();
 });
+
+$('[id *= "page-"').change(function(){
+	page = $(this).attr("id").split("-")[1];
+	if(location.search('page='))
+	location.href = location.href.replace(location.search, 'page=') + '&page=' + page;
+});
