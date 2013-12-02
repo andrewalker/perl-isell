4be7b643552dd2dd3aa2b45497f8ec185ca377b5
André Walker <andre@andrewalker.net>
2013-12-02 04:05:26 -0200

    Move javascript

--- root/static/js/scripts.js
+++ root/static/js/scripts.js
@@ -1,1 +1,4 @@
 /* scripts */
+$('#orderby').change(function () {
+    location.href = location.search ? location.href + '&sort=' + $('#orderby').val() : location.href + '?sort=' + $('#orderby').val();
+});
