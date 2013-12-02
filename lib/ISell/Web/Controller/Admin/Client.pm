51ab9dfcad3762bd7ec2405642415d203d53326b
André Walker <andre@andrewalker.net>
2013-12-02 06:19:47 -0200

    Fix client in admin

--- lib/ISell/Web/Controller/Admin/Client.pm
+++ lib/ISell/Web/Controller/Admin/Client.pm
@@ -4,21 +4,23 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller' };
 
-sub list :Chained('/admin/auth/base') PathPart('') Args(0) {
+sub list :Chained('/admin/auth/base') PathPart('client/list') Args(0) {
     my ($self, $ctx) = @_;
+
+    my @all = $ctx->model('DB::Client')->search->all;
 
     $ctx->stash(
         template => 'admin/client/list.tx',
         clients => [
             map {
-                {
+                +{
                     id    => $_->id,
                     cpf   => $_->cpf,
                     name  => $_->first_name . ' ' . $_->last_name,
                     email => $_->email,
                     sales => $_->shopping_orders->count,
                 }
-            } $ctx->model('DB::Client')->all
+            } @all
         ],
     );
 }
