f5fa5100e2fd209365cd4f12bed5d874ca2bd676
André Walker <andre@andrewalker.net>
2014-04-13 17:03:51 -0300

    Make cart a hashref in the session

--- lib/ISell/Web/Controller/ShoppingCart.pm
+++ lib/ISell/Web/Controller/ShoppingCart.pm
@@ -17,11 +17,14 @@ sub base : Chained('/') PathPrefix CaptureArgs(0) {
 sub add : Chained('base') Args(1) {
     my ($self, $ctx, $id) = @_;
 
-    my $cart = $ctx->session->{cart} || [];
+    my %cart = { $ctx->session->{cart} || {} };
 
-    push @$cart, $id;
+    $cart{$id} = {
+        created_at => time(),
+        quantity => 1,
+    };
 
-    $ctx->session->{cart} = $cart;
+    $ctx->session->{cart} = \%cart;
 
     $ctx->res->redirect(
         $ctx->uri_for(
@@ -34,17 +37,11 @@ sub add : Chained('base') Args(1) {
 sub remove : Chained('base') Args(1) {
     my ($self, $ctx, $id) = @_;
 
-    my @old_cart = @{ $ctx->session->{cart} || [] };
+    my %cart = %{ $ctx->session->{cart} || {} };
 
-    my @new_cart;
+    delete $cart{$id};
 
-    for my $item (@old_cart) {
-        if ($item != $id) {
-            push @new_cart, $item;
-        }
-    }
-
-    $ctx->session->{cart} = \@new_cart;
+    $ctx->session->{cart} = \%cart;
 
     $ctx->res->redirect(
         $ctx->uri_for(
@@ -57,7 +54,7 @@ sub remove : Chained('base') Args(1) {
 sub clear : Chained('base') Args(0) {
     my ($self, $ctx) = @_;
 
-    $ctx->session->{cart} = [];
+    $ctx->session->{cart} = {};
 
     $ctx->res->redirect(
         $ctx->uri_for(
@@ -73,13 +70,16 @@ sub list : Chained('base') PathPart('') Args(0) {
 
     $ctx->load_status_msgs;
 
-    my @cart = @{ $ctx->session->{cart} || [] };
-    my @items = map { $ctx->model('DB::Product')->find($_) } @cart;
+    my %cart = %{ $ctx->session->{cart} || {} };
+
+    my @items = map { $ctx->model('DB::Product')->find($cart{$_}) }
+                sort { $a->{created_at} <=> $b->{created_at} }
+                keys %cart;
 
     my $total = 0;
 
     for (@items) {
-        $total += $_->price;
+        $total += $_->price * $cart{$_->id}{quantity};
     }
 
     $ctx->stash(
@@ -97,25 +97,27 @@ sub checkout : Chained('base') Args(0) {
         created_at => \"now()",
     });
 
-    for (@{ $ctx->session->{cart} || [] }) {
+    my %cart  = %{ $ctx->session->{cart} || {} };
+
+    my @items = map { $ctx->model('DB::Product')->find($cart{$_}) }
+                sort { $a->{created_at} <=> $b->{created_at} }
+                keys %cart;
+
+    for (@items) {
         $order->add_to_shopping_order_items({
-            product_id => $_,
-            quantity => 1,
+            product_id => $_->id,
+            quantity => $cart{$_->id}{quantity},
         });
     }
-
-    my @cart = @{ $ctx->session->{cart} || [] };
-    my @items = map { $ctx->model('DB::Product')->find($_) } @cart;
 
     my $total = 0;
 
     for (@items) {
-        $total += $_->price;
+        $total += $_->price * $cart{$_->id}{quantity};
     }
 
-    $ctx->session->{cart} = [];
+    $ctx->session->{cart} = {};
     $ctx->stash( template => 'checkout.tx', items => \@items, total => $total );
-
 }
 
 __PACKAGE__->meta->make_immutable;
